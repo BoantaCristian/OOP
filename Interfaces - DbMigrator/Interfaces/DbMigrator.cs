9bf3677afdc6d382f3ae39ef0df860825ef34436
crist <cristi_cristi222@outlook.com>
2021-04-19 16:30:33 +0300

    DbMigrator featuring Interfaces

---
+++ Interfaces - DbMigrator/Interfaces/DbMigrator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public class DbMigrator
+    {
+        private readonly ILogger _logger;
+
+        public DbMigrator(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Migrate()
+        {
+            _logger.LogInfo("Migration started at "+ DateTime.UtcNow);
+            
+            Thread.Sleep(2000);
+
+            _logger.LogInfo("Migration finished at "+ DateTime.UtcNow);
+        }
+    }
+}
---
+++ Interfaces - DbMigrator/Interfaces/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbMigrator = new DbMigrator(new ConsoleLogger());
+            var dbMigrator2 = new DbMigrator(new FileLogger("C:\\Users\\crist\\OneDrive\\Desktop\\log.txt"));
+            dbMigrator.Migrate();
+            dbMigrator2.Migrate();
+            Console.Read();
+        }
+    }
+}
